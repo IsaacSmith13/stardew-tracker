3d88828bd67177186fd1650dbf90696c5a8c9319
IsaacSmith13 <Isaac13Smith@gmail.com>
2019-04-28 12:47:22 -0400

    Locked app to portrait mode; initialized empty database for the lists of items

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,7 @@
 var app = new Framework7({
-  // App root element
   root: '#app',
-  // App Name
   name: 'stardew-tracker',
-  // App id
   id: 'com.isaactsmith.stardewtracker',
-  // Enable swipe panel
-  panel: {
-    swipe: 'left',
-  },
-  // Add default routes
-  routes: [
-    {
-    },
-  ],
-  // ... other parameters
 });
 
 var mainView = app.views.create('.view-main');
---
+++ www/js/database.js
@@ -0,0 +1,11 @@
+var database = [
+    {
+        name: "Blueberry",
+        desc: "Seeds at Pierre's Shop",
+        season: ["summer"],
+        type: "farming",
+        img: "www/img/items/Blueberry.png"
+    }
+];
+
+var items = []
