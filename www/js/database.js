3461b7a96f056bc3aa1cd8f7d1dbb0de8707f2ed
IsaacSmith13 <Isaac13Smith@gmail.com>
2019-04-28 16:34:53 -0400

    App now uses localStorage to remember collected items

--- www/js/database.js
+++ www/js/database.js
@@ -1,61 +1,109 @@
 // Database of all items
-var database = [
+let database = [
     {
         name: "Blueberry",
         desc: "Seeds at Pierre's Shop",
         season: ["summer"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: ""
+        type: "farming"
     },
     {
-        name: "Blueberry",
+        name: "Hot Pepper",
         desc: "Seeds at Pierre's Shop",
-        season: ["any"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: ""
+        season: ["summer"],
+        type: "farming"
     },
     {
-        name: "Blueberry",
+        name: "Parsnip",
         desc: "Seeds at Pierre's Shop",
-        season: ["spring", "fall", "winter"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: "checked"
+        season: ["spring"],
+        type: "farming"
     },
     {
-        name: "Blueberry",
+        name: "Green Bean",
+        desc: "Seeds at Pierre's Shop",
+        season: ["spring"],
+        type: "farming"
+    },
+    {
+        name: "Cauliflower",
+        desc: "Seeds at Pierre's Shop",
+        season: ["spring"],
+        type: "farming"
+    },
+    {
+        name: "Potato",
+        desc: "Seeds at Pierre's Shop",
+        season: ["spring"],
+        type: "farming"
+    },
+    {
+        name: "Tomato",
+        desc: "Seeds at Pierre's Shop",
+        season: ["summer"],
+        type: "farming"
+    },
+    {
+        name: "Melon",
+        desc: "Seeds at Pierre's Shop",
+        season: ["summer"],
+        type: "farming"
+    },
+    {
+        name: "Corn",
         desc: "Seeds at Pierre's Shop",
         season: ["summer", "fall"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: "checked"
+        type: "farming"
     },
     {
-        name: "Blueberry",
+        name: "Eggplant",
         desc: "Seeds at Pierre's Shop",
-        season: ["summer", "any"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: "checked"
+        season: ["fall"],
+        type: "farming"
     },
     {
-        name: "Blueberry",
+        name: "Pumpkin",
         desc: "Seeds at Pierre's Shop",
-        season: ["winter", "any"],
-        type: "farming",
-        img: "img/items/Blueberry.png",
-        checked: "checked"
+        season: ["fall"],
+        type: "farming"
     },
+    {
+        name: "Yam",
+        desc: "Seeds at Pierre's Shop",
+        season: ["fall"],
+        type: "farming"
+    }
 ];
+
+// Add images, formatted names, and ID's to items systematically
+database.forEach(function (item) {
+    let count = 0;
+    item.img = `img/items/${item.name.split(" ").join("")}.png`;
+    item.formattedName = item.name.split(" ").join("");
+    item._id = ("0" + count).slice(-2);
+    count++
+});
+
+
+// If user doesn't yet have a save file
+if (localStorage.getItem("save") === null) {
+    console.log("null save");
+    // Create a save file for them
+    initialSave = {};
+    database.forEach(function (item) {
+        initialSave[item.formattedName] = ""
+    });
+    localStorage.setItem("save", JSON.stringify(initialSave));
+}
+// Either way, load the save file for later usage
+saveFile = JSON.parse(localStorage.getItem("save"));
 
 
 // Given a season and the id of an HTML list,
 // populates that list with all relevant items from the database;
 // also marking them as checked if applicable
 function populate(season, listId) {
-    seasonContent = `
+    let count = 0;
+    let seasonContent = `
         <li>
             <div class="item-content desc no-padding-left">
                 <div class="item-inner">
@@ -77,6 +125,7 @@ function populate(season, listId) {
     `
     // Iterates through all items and adding the correctly season-tagged ones
     database.forEach(function (item) {
+        count++;
         if (item.season.includes(season)) {
             seasonContent += `
             <li>
@@ -93,14 +142,15 @@ function populate(season, listId) {
                         </div>
                         <div class="item-after">
                             <label class="checkbox">
-                                <input type="checkbox" ${item.checked}>
+                                <input id="item${item.formattedName}" type="checkbox" ${saveFile[item.formattedName]}>
                                 <i class="icon-checkbox"></i>
                             </label>
                         </div>
                     </div>
                 </div>
             </li>
-        `}
+            `
+        }
     });
     // Assign generated content to the specified listId
     document.getElementById(listId).innerHTML = seasonContent;
@@ -113,3 +163,14 @@ populate("summer", "summerList");
 populate("fall", "fallList");
 populate("winter", "winterList");
 populate("any", "anyList");
+
+
+// Add click listeners to checkboxes to update user save file
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("click", function () {
+        let checked = checkbox.id.slice(4);
+        saveFile[checked] = (saveFile[checked] === "" ? "checked" : "");
+        localStorage.setItem("save", JSON.stringify(saveFile));
+    });
+});
