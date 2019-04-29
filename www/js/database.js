a46dd2050e62ae0ab92e78120a5b760daf134653
IsaacSmith13 <Isaac13Smith@gmail.com>
2019-04-29 17:59:25 -0400

    Tab generation overhauled to be more efficient and sort by category

--- www/js/database.js
+++ www/js/database.js
@@ -4,73 +4,73 @@ let database = [
         name: "Parsnip",
         desc: "Seeds at Pierre's Shop",
         season: ["spring"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Green Bean",
         desc: "Seeds at Pierre's Shop",
         season: ["spring"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Cauliflower",
         desc: "Seeds at Pierre's Shop",
         season: ["spring"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Potato",
         desc: "Seeds at Pierre's Shop",
         season: ["spring"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Tomato",
         desc: "Seeds at Pierre's Shop",
         season: ["summer"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Melon",
         desc: "Seeds at Pierre's Shop",
         season: ["summer"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Blueberry",
         desc: "Seeds at Pierre's Shop",
         season: ["summer"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Hot Pepper",
         desc: "Seeds at Pierre's Shop",
         season: ["summer"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Corn",
         desc: "Seeds at Pierre's Shop",
         season: ["summer", "fall"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Eggplant",
         desc: "Seeds at Pierre's Shop",
         season: ["fall"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Pumpkin",
         desc: "Seeds at Pierre's Shop",
         season: ["fall"],
-        type: "farming"
+        category: "farming"
     },
     {
         name: "Yam",
         desc: "Seeds at Pierre's Shop",
         season: ["fall"],
-        type: "farming"
+        category: "farming"
     }
 ];
 
@@ -93,57 +93,87 @@ if (localStorage.getItem("save") === null) {
     localStorage.setItem("save", JSON.stringify(initialSave));
 }
 // Either way, load the save file for later usage
-saveFile = JSON.parse(localStorage.getItem("save"));
+let saveFile = JSON.parse(localStorage.getItem("save"));
 
 
-// Given a season and the id of an HTML list,
-// populates that list with all relevant items from the database;
-// also marking them as checked if applicable
-function populate(season, listId) {
-    let count = 0;
-    let seasonContent = ""
+const seasonList = ["spring", "summer", "fall", "winter", "any"];
+const categoryList = ["farming", "foraging", "fishing", "mining", "misc"];
 
-    // Iterates through all items and adding the correctly season-tagged ones
-    database.forEach(function (item) {
-        count++;
-        if (item.season.includes(season)) {
-            seasonContent += `
-            <li class="shownLi">
-                <div class="item-content no-padding">
-                    <div class="item-inner">
-                        <div class="item-media">
-                            <img src="${item.img}" width="48" height="48">
-                        </div>
-                        <div class="item-title">
-                            <button class="button-name button${item.formattedName}">${item.name}<i class="icon-down-open"></i></button>
-                        </div>
-                        <div class="item-after">
-                            <label class="checkbox">
-                                <input class="check${item.formattedName}" type="checkbox" ${saveFile[item.formattedName]}>
-                                <i class="icon-checkbox"></i>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li class="hidden">
-                <div class="desc">
-                    ${item.desc}
-                </div>
-            </li>
-        `}
-    });
-    // Assign generated content to the specified listId
-    document.getElementById(listId).innerHTML = seasonContent;
+
+// Capitalizes the first letter of a string
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 
-// Call populate function on all tabs
-populate("spring", "springList");
-populate("summer", "summerList");
-populate("fall", "fallList");
-populate("winter", "winterList");
-populate("any", "anyList");
+// Populates all tabs with content from the database
+function populate(seasonList, categoryList) {
+    // Iterates through each season's tab
+    seasonList.forEach(function (season) {
+        let seasonContent = "";
+        // Iterates through each category
+        categoryList.forEach(function (category) {
+            // Winter contains no farming items, so skip the category
+            if (season === "winter" && category === "farming") {
+                return;
+            }
+            // Only "any" has mining and misc, so skip them otherwise
+            if (season !== "any" && category === "mining") {
+                return;
+            }
+            if (season !== "any" && category === "misc") {
+                return;
+            }
+            // Announce the catagory and start a list
+            let categoryContent = `
+            <div class="list">
+                <h3 class="text-align-center"><strong>${capitalizeFirstLetter(category)}:</strong></h3>
+                <ul class="${season}List">`;
+            // Iterate through the database
+            database.forEach(function (item) {
+                // If the item is in this season AND catagory, add it to the list
+                if (item.season.includes(season) && item.category === category) {
+                    categoryContent += `
+                    <li class="shownLi">
+                        <div class="item-content no-padding">
+                            <div class="item-inner">
+                                <div class="item-media">
+                                    <img src="${item.img}" width="48" height="48">
+                                </div>
+                                <div class="item-title">
+                                    <button class="button-name button${item.formattedName}">${item.name}<i class="icon-down-open"></i></button>
+                                </div>
+                                <div class="item-after">
+                                    <label class="checkbox">
+                                        <input class="check${item.formattedName}" type="checkbox" ${saveFile[item.formattedName]}>
+                                        <i class="icon-checkbox"></i>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="hidden">
+                        <div class="desc">
+                            ${item.desc}
+                        </div>
+                    </li>`
+                }
+            });
+            // End the list
+            categoryContent += `
+                </ul>
+            </div>`;
+            // Append this catagory to the season
+            seasonContent += categoryContent;
+        });
+        // Change the content of this season's tab to the HTML created by the function
+        document.getElementById(season).innerHTML = seasonContent;
+    });
+}
+
+
+// Calls the populate function to fill the tabs
+populate(seasonList, categoryList);
 
 
 // Add click listeners to checkboxes to update user save file
